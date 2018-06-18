8fe6b10320c25508c01ed7f2a6f3a3e9e11fc905
Amy Yates <yateam@ohsu.edu>
2018-06-18 13:38:53 -0700

    Fix file formatting.

--- src/main/java/org/monarchinitiative/fhir2hpo/loinc/LoincId.java
+++ src/main/java/org/monarchinitiative/fhir2hpo/loinc/LoincId.java
@@ -4,46 +4,48 @@ import org.monarchinitiative.fhir2hpo.loinc.exception.LoincException;
 import org.monarchinitiative.fhir2hpo.loinc.exception.LoincFormatException;
 
 public class LoincId {
-	
+
 	String code;
-	
+
 	/**
 	 * Construct a LoincId and throw an exception if the code is not understood.
+	 * 
 	 * @param code
-	 * @throws LoincException the exception thrown if the code is not of the format "i-j" where i and j are integers
+	 * @throws LoincException
+	 *             the exception thrown if the code is not of the format "i-j" where i and j are integers
 	 */
 	public LoincId(String code) throws LoincException {
-		
+
 		this.code = code;
 		if (!code.matches("^\\d+-\\d+$")) {
 			throw new LoincFormatException("The code " + code + " is not a valid LOINC format.");
 		}
 	}
-	
+
 	public String getCode() {
 		return code;
 	}
-	
-    @Override
+
+	@Override
 	public int hashCode() {
 		return code.hashCode();
 	}
 
-    @Override
-    public boolean equals(Object o) {
-        if (o == this) {
+	@Override
+	public boolean equals(Object o) {
+		if (o == this) {
 			return true;
 		}
-        if (!(o instanceof LoincId)) {
-            return false;
-        }
-        LoincId other = (LoincId) o;
-        return this.code.equals(other.getCode());
-    }
-    
-    @Override
-    public String toString() {
-    	return code;
-    }
+		if (!(o instanceof LoincId)) {
+			return false;
+		}
+		LoincId other = (LoincId) o;
+		return this.code.equals(other.getCode());
+	}
+
+	@Override
+	public String toString() {
+		return code;
+	}
 
 }
